2d25c2cccda88f99f8d7e139e01854e0fd1cd508
marinawlevy <marinawlevy@gmail.com>
2026-09-22 10:00:18 -0700

    edit to readme and added analysis file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 this is a demo for stat545
 
 Marina Levy is a M.Sc. student in IRES
+
+this is an edit I made in RStudio
---
+++ analysis.R
@@ -0,0 +1,7 @@
+#view the first few rows of mtcars dataset 
+head(mtcars)
+#calculate the average mpg among all cars and save it as a variable
+mean_mpg <- mean(mtcars$mpg)
+
+#print out the mean 
+mean_mpg
