9b94d3e427c4c8dbe5d62bc29d76199096bac058
marinawlevy <marinawlevy@gmail.com>
2026-09-22 10:19:20 -0700

    create high mpg dataset of cars with a higher-than-average mpg

--- analysis.R
+++ analysis.R
@@ -1,7 +1,15 @@
-#view the first few rows of mtcars dataset 
-head(mtcars)
+#install.packages("dplyr")
+library(dplyr)
+
 #calculate the average mpg among all cars and save it as a variable
 mean_mpg <- mean(mtcars$mpg)
 
 #print out the mean 
 mean_mpg
+
+#new for cars that have an mpg greater than the average and save as high mpg
+high_mpg <- dplyr::filter(mtcars,mpg > mean_mpg)
+
+#show head of high mpg
+head(high_mpg)
+
