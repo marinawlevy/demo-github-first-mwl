fa52aa202b1c522dac16698a35a481e9009fc657
marinawlevy <marinawlevy@gmail.com>
2026-09-24 09:18:20 -0700

    edited comments

--- analysis.R
+++ analysis.R
@@ -1,4 +1,5 @@
 #install.packages("dplyr")
+
 library(dplyr)
 
 #calculate the average mpg among all cars and save it as a variable
@@ -7,7 +8,7 @@ mean_mpg <- mean(mtcars$mpg)
 #print out the mean 
 mean_mpg
 
-#new for cars that have an mpg less than the average and save as high mpg
+#new dataframe for cars that have an mpg less than the average 
 low_mpg <- dplyr::filter(mtcars,mpg < mean_mpg)
 
 #show head of low mpg
