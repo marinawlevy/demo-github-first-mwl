31a7317bd41c2ec01d0c945a21d1d36d3cc5aed1
marinawlevy <marinawlevy@gmail.com>
2026-09-24 09:13:05 -0700

    changed from high mpg to low mpg

--- analysis.R
+++ analysis.R
@@ -7,9 +7,9 @@ mean_mpg <- mean(mtcars$mpg)
 #print out the mean 
 mean_mpg
 
-#new for cars that have an mpg greater than the average and save as high mpg
-high_mpg <- dplyr::filter(mtcars,mpg > mean_mpg)
+#new for cars that have an mpg less than the average and save as high mpg
+low_mpg <- dplyr::filter(mtcars,mpg < mean_mpg)
 
-#show head of high mpg
-head(high_mpg)
+#show head of low mpg
+head(low_mpg)
 
